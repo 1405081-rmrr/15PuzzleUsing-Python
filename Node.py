a04de9dd20bd92783f9cc53d8cc2b4ba1dccb842
Roktim Reza <1405081.rmrr@ugrad.cse.buet.ac.bd>
2020-04-30 13:09:22 +0600

    same as node.java

--- Node.py
+++ Node.py
@@ -1,14 +1,43 @@
 class Node :
+    cost=100000000000000
     def __init__(self,matrix,x,y,newX,newY,level,parent):
         self.parent=parent
         self.matrix=matrix
-        self.cost=100000000000000
         self.level=level
-        self.newX=newX
-        self.newY=newY
+    #def __repr__(self):
+        #return "Node(Level={} cost={})".format(self.level,self.cost)
+        for i in range(4):
+            for j in range(4):
+                self.matrix[i][j]=matrix[i][j]
+        self.matrix[x][y]=self.matrix[x][y]+self.matrix[newX][newY]
+        self.matrix[newX][newY]=self.matrix[x][y]-self.matrix[newX][newY]
+        self.matrix[x][y]=self.matrix[x][y]-self.matrix[newX][newY]
+        self.x=newX
+        self.y=newY
 
-        #self.matrix[x][y]=self.matrix[x][y]+self.matrix[newX][newY]
-        #self.matrix[newX][newY]=self.matrix[x][y]-self.matrix[newX][newY]
-        #self.matrix[x][y]=self.matrix[x][y]-self.matrix[newX][newY]
+class ComparableNode:
+    def __init__(self, cost,level,matrix,x,y,x1,y1,parent):
+            self.cost=cost
+            self.level=level
+            self.matrix=matrix
+            self.x=x1
+            self.y=y1
+            self.parent=parent
 
-mat = [[0 for i in range(3)] for j in range(3)]
+            #self.matrix[x][y] = self.matrix[x][y] + self.matrix[x1][y1]
+            #self.matrix[x1][y1] = self.matrix[x][y] - self.matrix[x1][y1]
+            #self.matrix[x][y] = self.matrix[x][y] - self.matrix[x1][y1]
+
+
+    def __gt__(self, other):
+        # x = self.node.cost + self.node.level
+        # y = other.node.cost + other.node.level
+        # return x > y
+        p=self.cost+self.level
+        q=other.level+other.cost
+        return p>q
+
+
+    #def __repr__(self):
+        #return repr(self.matrix)
+         #return self.matrix
