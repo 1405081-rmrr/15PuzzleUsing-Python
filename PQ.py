e11a0d846b93213b0572d190c4f23f0aad697ec1
Roktim Reza <1405081.rmrr@ugrad.cse.buet.ac.bd>
2020-04-30 13:09:46 +0600

    practice

--- PQ.py
+++ PQ.py
@@ -4,6 +4,7 @@ class ComparableNode:
     def __init__(self, node):
         self.node = node
 
+
     def __gt__(self, other):
         x = self.node.cost + self.node.level
         y = other.node.cost + other.node.level
